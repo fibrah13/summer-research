f8d35f4a26e83f0ad550cb41cadbdfdb7938b682
fibrah13 <fibrah13@earlham.edu>
2015-05-21 15:25:50 -0400

    new file

---
+++ summer_research.pl
@@ -0,0 +1,8 @@
+Summer Research
+
+The goal of this research is to work on Drug design for the Fibroblast Growth Factor using structural informatics.  This will be done computationally using the unix shell and the Practical Extraction and Reporting Language (PERL) as the programming language.  The program to be used in designing the drug is Hackamol::X::Vina.
+
+Week one: - Work on familiarizing myself with Perl, the Jupyter notebook and the unix shell. Started creating z matrices of amino acids.
+
+Week two: -  Continue constructing z matrices of the amino acids using coordinates from PDB and using VMD to visualize the structures. Also started learning to push and pull from github so there is always a back up of the files.
+
